3ab7b6f3f38cb0b2941a74f9fcbe07dfbb4b9905
zhongfw <1739174050@qq.com>
2024-02-20 14:15:29 +0800

    fix: 18in

--- docs/.vuepress/configs/navbar/en.ts
+++ docs/.vuepress/configs/navbar/en.ts
@@ -2,33 +2,63 @@ import type { NavbarConfig } from "vuepress-theme-gungnir";
 
 export const en: NavbarConfig = [
   {
-    text: "Home",
+    text: "首页",
     link: "/",
-    icon: "fa-fort-awesome"
+    icon: "fa-fort-awesome",
   },
   {
-    text: "Tags",
+    text: "标签",
     link: "/tags/",
-    icon: "fa-tag"
+    icon: "fa-tag",
   },
   {
-    text: "Links",
+    text: "链接",
     link: "/links/",
-    icon: "fa-satellite-dish"
+    icon: "fa-satellite-dish",
   },
   {
-    text: "Posts",
-    link: "/posts/readme.md",
-    icon: "ri-article-line"
+    text: "文档",
+    link: "/zh/posts/readme.md",
+    icon: "ri-article-line",
   },
   {
-    text: "DevDocs",
-    link: "/docs/basic/intro.md",
-    icon: "ri-book-2-fill"
+    text: "开发文档",
+    link: "/zh/docs/basic/intro.md",
+    icon: "ri-book-2-fill",
   },
   {
     text: "VuePress",
-    link: "https://v2.vuepress.vuejs.org/",
-    icon: "ri-vuejs-line"
-  }
+    link: "https://v2.vuepress.vuejs.org/zh/",
+    icon: "ri-vuejs-line",
+  },
+  // {
+  //   text: "Home",
+  //   link: "/",
+  //   icon: "fa-fort-awesome",
+  // },
+  // {
+  //   text: "Tags",
+  //   link: "/tags/",
+  //   icon: "fa-tag",
+  // },
+  // {
+  //   text: "Links",
+  //   link: "/links/",
+  //   icon: "fa-satellite-dish",
+  // },
+  // {
+  //   text: "Posts",
+  //   link: "/posts/readme.md",
+  //   icon: "ri-article-line",
+  // },
+  // {
+  //   text: "DevDocs",
+  //   link: "/docs/basic/intro.md",
+  //   icon: "ri-book-2-fill",
+  // },
+  // {
+  //   text: "VuePress",
+  //   link: "https://v2.vuepress.vuejs.org/",
+  //   icon: "ri-vuejs-line",
+  // },
 ];
