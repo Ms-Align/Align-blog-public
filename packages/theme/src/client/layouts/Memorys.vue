eb7f3865e9f8b856d6b61ce903c3d9a56b0cf7a2
zhongfw <1739174050@qq.com>
2024-03-14 11:17:37 +0800

    fix: api式的element组件打包会报错，很奇怪

--- packages/theme/src/client/layouts/Memorys.vue
+++ packages/theme/src/client/layouts/Memorys.vue
@@ -2,14 +2,24 @@
     <Common>
         <template #page>
             <PageHeader :page-info="pageInfo" />
-            <audio loop="true" ref="audioRef" autoplay="true" :src="`data:audio/mp3;base64,${TK}`
-                ">
+            <el-dialog v-model="dialogVisible" title="Memory提示" width="500" :before-close="handleClose">
+                <span>Memory模块我们特地提供了背景音乐，希望你喜欢。</span>
+                <template #footer>
+                    <div class="dialog-footer">
+                        <el-button type="primary" @click="handleClose">
+                            开始吧
+                        </el-button>
+                    </div>
+                </template>
+            </el-dialog>
+            <audio loop="true" ref="audioRef" autoplay="true"
+                src="https://124.223.165.180/api/static/musics/376db03817986dd1bae7f5f206340fb9.mpeg">
             </audio>
             <div class="tags-wrapper">
                 <el-timeline>
-                    <el-timeline-item type='primary' hollow v-for="(record, index) in (frontmatter?.memorys || [])"
+                    <el-timeline-item type='primary' hollow v-for="( record, index ) in  (frontmatter?.memorys || []) "
                         :timestamp="record?.date" placement="top">
-                        <div v-for="(memory, index) in record?.memory">
+                        <div v-for="( memory, index ) in  record?.memory ">
                             <el-card>
                                 <template #header>
                                     <div class="card-header">
@@ -29,7 +39,7 @@
                                     </el-col>
                                 </el-row>
                                 <el-row style="padding-top: 16px;" :gutter="6" v-if="memory?.img?.length">
-                                    <el-col :span="6" v-for="(src, index) in memory?.img">
+                                    <el-col :span="6" v-for="( src, index ) in  memory?.img ">
                                         <el-image :preview-src-list="memory?.img" :src="src" :zoom-rate="1.2"
                                             :max-scale="7" :min-scale="0.2" :initial-index="4" fit="cover" />
                                     </el-col>
@@ -58,7 +68,7 @@
 import Common from "@theme/Common.vue";
 import PageHeader from "@theme/PageHeader.vue";
 import { usePageFrontmatter } from "@vuepress/client";
-import { TK } from '../../../public/tk';
+//import { TK } from '../../../public/tk';
 import { computed, ref, onMounted } from "vue";
 import type {
     GungnirThemeLinksPageFrontmatter,
@@ -78,13 +88,19 @@ const pageInfo = computed(() => {
     if (info.title === undefined) info.title = themeLocale.value.pageText?.memorys;
     return info;
 });
-ElMessageBox.alert('我们为Memory提供了背景音乐，希望你喜欢。', 'Align', {
-    // if you want to disable its autofocus
-    // autofocus: false,
-    confirmButtonText: '开始吧！',
-    callback: (action: any) => {
-        (audioRef.value as any).play()
-    },
-})
+const dialogVisible = ref(true)
+
+const handleClose = (done: () => void) => {
+    (audioRef.value as any).play()
+    dialogVisible.value = false
+}
+// ElMessageBox.alert('我们为Memory提供了背景音乐，希望你喜欢。', 'Align', {
+//     // if you want to disable its autofocus
+//     // autofocus: false,
+//     confirmButtonText: '开始吧！',
+//     callback: (action: any) => {
+//         (audioRef.value as any).play()
+//     },
+// })
 
 </script>
