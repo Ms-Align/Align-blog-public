2c255f3659d4e73776f4edf1988c4fd06747519d
zhongfw <1739174050@qq.com>
2024-03-22 09:42:16 +0800

    fix: 修改英文文本为中文

--- packages/theme/src/node/i18n/en.ts
+++ packages/theme/src/node/i18n/en.ts
@@ -1,66 +1,129 @@
 import type { GungnirThemeI18n } from "../../shared";
 
-const en: GungnirThemeI18n = {
+ const en: GungnirThemeI18n = 
+//   {
+//   // navbar
+//   navbarTitle: "Align",
+
+//   // language selector
+//   selectLanguageName: "English",
+//   selectLanguageText: "Languages",
+//   selectLanguageAriaLabel: "Select language",
+
+//   // page meta
+//   editLinkText: "Edit this page on GitHub",
+//   lastUpdatedText: "Last Updated",
+//   contributorsText: "Contributors",
+
+//   // search
+//   searchText: "Search",
+
+//   // custom containers
+//   info: "INFO",
+//   tip: "TIP",
+//   warning: "WARNING",
+//   danger: "DANGER",
+
+//   // 404 page
+//   notFound: [
+//     `There's nothing here.`,
+//     `How did we get here?`,
+//     `That's a Four-Oh-Four.`,
+//     `Looks like we've got some broken links.`
+//   ],
+//   backToHome: "$ cd /home/",
+
+//   // a11y
+//   openInNewWindow: "open in new window",
+//   toggleColorMode: "toggle color mode",
+//   toggleSidebar: "toggle sidebar",
+
+//   // other pages
+//   pageText: {
+//     tags: "Tags",
+//     links: "Links",
+//     memorys:"Memorys"
+//   },
+
+//   // "show all tags" button
+//   showAllTagsText: "Show All",
+
+//   // post pager
+//   postNext: "Next",
+//   postPrev: "Previous",
+
+//   // home page pager
+//   homeNext: "Newer Posts",
+//   homePrev: "Older Posts",
+
+//   // PWA-popup
+//   pwaPopupMessage: "New content is available.",
+//   pwaPopupButtonText: "Refresh",
+
+//   // header image
+//   headerImageCredit: "Image by"
+// };
+{
   // navbar
   navbarTitle: "Align",
 
   // language selector
-  selectLanguageName: "English",
-  selectLanguageText: "Languages",
-  selectLanguageAriaLabel: "Select language",
+  selectLanguageName: "简体中文",
+  selectLanguageText: "选择语言",
+  selectLanguageAriaLabel: "选择语言",
 
   // page meta
-  editLinkText: "Edit this page on GitHub",
-  lastUpdatedText: "Last Updated",
-  contributorsText: "Contributors",
+  editLinkText: "在 GitHub 上编辑此页",
+  lastUpdatedText: "上次更新",
+  contributorsText: "贡献者",
 
   // search
-  searchText: "Search",
+  searchText: "搜索",
 
   // custom containers
-  info: "INFO",
-  tip: "TIP",
-  warning: "WARNING",
-  danger: "DANGER",
+  info: "信息",
+  tip: "提示",
+  warning: "注意",
+  danger: "警告",
 
   // 404 page
   notFound: [
-    `There's nothing here.`,
-    `How did we get here?`,
-    `That's a Four-Oh-Four.`,
-    `Looks like we've got some broken links.`
+    "这里什么都没有",
+    "我们怎么到这来了？",
+    "这是一个 404 页面",
+    "看起来我们进入了错误的链接"
   ],
   backToHome: "$ cd /home/",
 
   // a11y
-  openInNewWindow: "open in new window",
-  toggleColorMode: "toggle color mode",
-  toggleSidebar: "toggle sidebar",
+  openInNewWindow: "在新窗口打开",
+  toggleColorMode: "切换颜色模式",
+  toggleSidebar: "切换侧边栏",
 
   // other pages
   pageText: {
-    tags: "Tags",
-    links: "Links",
-    memorys:"Memorys"
+    tags: "标签",
+    links: "链接",
+    memorys:"记忆"
   },
 
   // "show all tags" button
-  showAllTagsText: "Show All",
+  showAllTagsText: "全部",
 
   // post pager
-  postNext: "Next",
-  postPrev: "Previous",
+  postNext: "下一篇",
+  postPrev: "上一篇",
 
   // home page pager
-  homeNext: "Newer Posts",
-  homePrev: "Older Posts",
+  homeNext: "较新的文章",
+  homePrev: "较旧的文章",
 
   // PWA-popup
-  pwaPopupMessage: "New content is available.",
-  pwaPopupButtonText: "Refresh",
+  pwaPopupMessage: "发现有新内容更新",
+  pwaPopupButtonText: "刷新",
 
   // header image
-  headerImageCredit: "Image by"
+  headerImageCredit: "图源"
 };
 
 export { en };
