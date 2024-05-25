b20a971434e5a21e15e671e8960a5ae6fcc8a509
zhongfw <1739174050@qq.com>
2024-05-25 16:36:43 +0800

    fix: 1

--- docs/.vuepress/public/sensorInit.js
+++ docs/.vuepress/public/sensorInit.js
@@ -2,10 +2,10 @@
 var sensors = window["sensorsDataAnalytic201505"];
 sensors.init({
   server_url:
-    "http://116.196.65.10/receiver/api/gp?project=clklogapp&token=5388ed7459ba4c4cad0c8693fb85630a",
+    "https://116.196.65.10/receiver/api/gp?project=clklogapp&token=5388ed7459ba4c4cad0c8693fb85630a",
   is_track_single_page: true, // 单页面配置，默认开启，若页面中有锚点设计，需要将该配置删除，否则触发锚点会多触发 $pageview 事件
   use_client_time: true,
-  show_log: true,
+  // show_log: true,
   send_type: "beacon",
   heatmap: {
     // eslint-disable-next-line spaced-comment
